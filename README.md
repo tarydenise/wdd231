317c3729df0b68c32a1d4a96a11b59f0a9172aab
tarydenise <tarydenise@gmail.com>
2025-01-15 02:04:59 -0800

    W01 Prove National Park Service 1

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ Use this as a starting point to complete the WDD 231 personal activity. We will 
 ## Other commands
 
 - `npm run build` to build final files when you are ready to turn in.
+
+## Netlify URL
+
+https://regal-bunny-a7ad26.netlify.app/
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+document.title = parkData.fullName;
+
+const heroImage = document.querySelector(".hero-banner > img");
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-banner_title">${info.name}</a>
+    <p class="hero-banner_subtitle">
+        <span>${info.designation}</span>
+        <span>${info.states}</span>
+    </p>`;
+}
+
+const heroContent = document.querySelector(".hero-banner_content");
+heroContent.innerHTML = parkInfoTemplate(parkData);
